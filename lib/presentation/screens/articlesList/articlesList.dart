f8a8f3dbe4897013dd0746d60b41892caf568e8a
Sam Victor <iamvictorsam@gmail.com>
2023-09-12 14:33:58 +0100

    finished Article screen

--- lib/presentation/screens/articlesList/articlesList.dart
+++ lib/presentation/screens/articlesList/articlesList.dart
@@ -1,8 +1,5 @@
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:get/utils.dart';
-import 'package:heavy_metals/constant.dart';
 
 class ArticleListScreen extends StatelessWidget {
   const ArticleListScreen({super.key});
