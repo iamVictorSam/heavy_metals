33c94d5c4a3609b62120b4395ce273cb05884df0
Sam Victor <iamvictorsam@gmail.com>
2023-09-12 14:36:24 +0100

    finished Article screen

---
+++ lib/presentation/screens/search/widget/heavy_metal_dropdown.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:heavy_metals/constant.dart';
+
+class HeavyMetalsDropdown extends StatefulWidget {
+  const HeavyMetalsDropdown({super.key});
+
+  @override
+  _HeavyMetalsDropdownState createState() => _HeavyMetalsDropdownState();
+}
+
+class _HeavyMetalsDropdownState extends State<HeavyMetalsDropdown> {
+  String? selectedMetal;
+  List<String> metals = ['Lead', 'Mercury', 'Cadmium', 'Arsenic', 'Chromium'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12.0),
+          child: DropdownButton<String>(
+            isExpanded: true,
+            underline: Container(),
+            value: selectedMetal,
+            onChanged: (String? newValue) {
+              setState(() {
+                selectedMetal = newValue;
+              });
+            },
+            items: metals.map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Row(
+                  children: [
+                    const Icon(Icons.category_outlined, color: kPrimaryColor),
+                    const SizedBox(width: 8),
+                    Text(value),
+                  ],
+                ),
+              );
+            }).toList(),
+          ),
+        ),
+      ),
+    );
+  }
+}
