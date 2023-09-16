c4c4099d2925928043446c830c293d0e564bdbee
Sam Victor <iamvictorsam@gmail.com>
2023-09-16 15:37:00 +0100

    done

--- lib/presentation/screens/home.dart
+++ lib/presentation/screens/home.dart
@@ -10,56 +10,54 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Stack(children: [
-          FlutterMap(
-            options: MapOptions(
-              center: const LatLng(51.509364, -0.128928),
-              zoom: 9.2,
-            ),
-            nonRotatedChildren: const [],
-            children: [
-              TileLayer(
-                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                userAgentPackageName: 'com.example.app',
-              ),
-            ],
+    return Scaffold(
+      body: Stack(children: [
+        FlutterMap(
+          options: MapOptions(
+            center: const LatLng(51.509364, -0.128928),
+            zoom: 9.2,
           ),
-          Positioned(
-            top: 20,
-            left: 10,
-            right: 10,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
+          nonRotatedChildren: const [],
+          children: [
+            TileLayer(
+              urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+              userAgentPackageName: 'com.example.app',
+            ),
+          ],
+        ),
+        Positioned(
+          top: 80,
+          left: 10,
+          right: 10,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: ListTile(
+              leading: Icon(
+                EvaIcons.search,
+                color: greyColor,
               ),
-              child: ListTile(
-                leading: Icon(
-                  EvaIcons.search,
+              title: const TextField(
+                decoration: InputDecoration(
+                  enabled: false,
+                  hintText: 'Search location or meta',
+                  border: InputBorder.none,
+                ),
+              ),
+              trailing: GestureDetector(
+                onTap: () {},
+                child: Icon(
+                  Icons.close,
+                  size: 18,
                   color: greyColor,
                 ),
-                title: const TextField(
-                  decoration: InputDecoration(
-                    enabled: false,
-                    hintText: 'Search location or meta',
-                    border: InputBorder.none,
-                  ),
-                ),
-                trailing: GestureDetector(
-                  onTap: () {},
-                  child: Icon(
-                    Icons.close,
-                    size: 18,
-                    color: greyColor,
-                  ),
-                ),
               ),
             ),
           ),
-        ]),
-      ),
+        ),
+      ]),
     );
   }
 }
