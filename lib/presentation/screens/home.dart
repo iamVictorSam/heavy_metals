fdb2430bb48adcf176c7775be839da0c669c2d77
Sam Victor <iamvictorsam@gmail.com>
2023-09-27 10:18:49 +0100

    fix

--- lib/presentation/screens/home.dart
+++ lib/presentation/screens/home.dart
@@ -68,64 +68,68 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return _homeController.isLoading.value
-        ? const Center(
-            child: CircularProgressIndicator(),
-          )
-        : Scaffold(
-            body: Stack(
-              children: [
-                FlutterMap(
-                  options: MapOptions(
-                    center: const LatLng(4.7339, 7.2300),
-                    zoom: 10.3,
+    // Calculate the total
+
+    return Obx(
+      () => _homeController.isLoading.value
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : Scaffold(
+              body: Stack(
+                children: [
+                  FlutterMap(
+                    options: MapOptions(
+                      center: const LatLng(4.7339, 7.2300),
+                      zoom: 10.3,
+                    ),
+                    nonRotatedChildren: const [],
+                    children: [
+                      TileLayer(
+                        urlTemplate:
+                            'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                        userAgentPackageName: 'com.example.app',
+                      ),
+                      MarkerLayer(
+                        markers: customMarkers,
+                      ),
+                    ],
                   ),
-                  nonRotatedChildren: const [],
-                  children: [
-                    TileLayer(
-                      urlTemplate:
-                          'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                      userAgentPackageName: 'com.example.app',
-                    ),
-                    MarkerLayer(
-                      markers: customMarkers,
-                    ),
-                  ],
-                ),
-                // Positioned(
-                //   top: 80,
-                //   left: 10,
-                //   right: 10,
-                //   child: Container(
-                //     decoration: BoxDecoration(
-                //       color: Colors.white,
-                //       borderRadius: BorderRadius.circular(10),
-                //     ),
-                //     child: ListTile(
-                //       leading: Icon(
-                //         EvaIcons.search,
-                //         color: greyColor,
-                //       ),
-                //       title: const TextField(
-                //         decoration: InputDecoration(
-                //           enabled: false,
-                //           hintText: 'Search location or meta',
-                //           border: InputBorder.none,
-                //         ),
-                //       ),
-                //       trailing: GestureDetector(
-                //         onTap: () {},
-                //         child: Icon(
-                //           Icons.close,
-                //           size: 18,
-                //           color: greyColor,
-                //         ),
-                //       ),
-                //     ),
-                //   ),
-                // ),
-              ],
+                  // Positioned(
+                  //   top: 80,
+                  //   left: 10,
+                  //   right: 10,
+                  //   child: Container(
+                  //     decoration: BoxDecoration(
+                  //       color: Colors.white,
+                  //       borderRadius: BorderRadius.circular(10),
+                  //     ),
+                  //     child: ListTile(
+                  //       leading: Icon(
+                  //         EvaIcons.search,
+                  //         color: greyColor,
+                  //       ),
+                  //       title: const TextField(
+                  //         decoration: InputDecoration(
+                  //           enabled: false,
+                  //           hintText: 'Search location or meta',
+                  //           border: InputBorder.none,
+                  //         ),
+                  //       ),
+                  //       trailing: GestureDetector(
+                  //         onTap: () {},
+                  //         child: Icon(
+                  //           Icons.close,
+                  //           size: 18,
+                  //           color: greyColor,
+                  //         ),
+                  //       ),
+                  //     ),
+                  //   ),
+                  // ),
+                ],
+              ),
             ),
-          );
+    );
   }
 }
