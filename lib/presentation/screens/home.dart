7845c1c10651f34c43a01f746b17233eb27fcd27
Sam Victor <iamvictorsam@gmail.com>
2023-09-27 09:57:27 +0100

    fix

--- lib/presentation/screens/home.dart
+++ lib/presentation/screens/home.dart
@@ -18,7 +18,12 @@ class _HomeScreenState extends State<HomeScreen> {
   late final customMarkers = <Marker>[
     // tai
     buildPin(const LatLng(4.7518, 7.2437),
-        cd: '49', pb: '53', ni: '43', cr: '68', hg: '38', zn: '60'),
+        cd: _homeController.metals[4].cd,
+        pb: _homeController.metals[4].pb,
+        ni: _homeController.metals[4].ni,
+        cr: '68',
+        hg: '38',
+        zn: '60'),
     // eleme
     buildPin(const LatLng(4.7994, 7.1198),
         cd: '49', pb: '53', ni: '43', cr: '68', hg: '38', zn: '60'),
@@ -63,59 +68,64 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          FlutterMap(
-            options: MapOptions(
-              center: const LatLng(4.7339, 7.2300),
-              zoom: 10.3,
+    return _homeController.isLoading.value
+        ? const Center(
+            child: CircularProgressIndicator(),
+          )
+        : Scaffold(
+            body: Stack(
+              children: [
+                FlutterMap(
+                  options: MapOptions(
+                    center: const LatLng(4.7339, 7.2300),
+                    zoom: 10.3,
+                  ),
+                  nonRotatedChildren: const [],
+                  children: [
+                    TileLayer(
+                      urlTemplate:
+                          'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                      userAgentPackageName: 'com.example.app',
+                    ),
+                    MarkerLayer(
+                      markers: customMarkers,
+                    ),
+                  ],
+                ),
+                // Positioned(
+                //   top: 80,
+                //   left: 10,
+                //   right: 10,
+                //   child: Container(
+                //     decoration: BoxDecoration(
+                //       color: Colors.white,
+                //       borderRadius: BorderRadius.circular(10),
+                //     ),
+                //     child: ListTile(
+                //       leading: Icon(
+                //         EvaIcons.search,
+                //         color: greyColor,
+                //       ),
+                //       title: const TextField(
+                //         decoration: InputDecoration(
+                //           enabled: false,
+                //           hintText: 'Search location or meta',
+                //           border: InputBorder.none,
+                //         ),
+                //       ),
+                //       trailing: GestureDetector(
+                //         onTap: () {},
+                //         child: Icon(
+                //           Icons.close,
+                //           size: 18,
+                //           color: greyColor,
+                //         ),
+                //       ),
+                //     ),
+                //   ),
+                // ),
+              ],
             ),
-            nonRotatedChildren: const [],
-            children: [
-              TileLayer(
-                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                userAgentPackageName: 'com.example.app',
-              ),
-              MarkerLayer(
-                markers: customMarkers,
-              ),
-            ],
-          ),
-          // Positioned(
-          //   top: 80,
-          //   left: 10,
-          //   right: 10,
-          //   child: Container(
-          //     decoration: BoxDecoration(
-          //       color: Colors.white,
-          //       borderRadius: BorderRadius.circular(10),
-          //     ),
-          //     child: ListTile(
-          //       leading: Icon(
-          //         EvaIcons.search,
-          //         color: greyColor,
-          //       ),
-          //       title: const TextField(
-          //         decoration: InputDecoration(
-          //           enabled: false,
-          //           hintText: 'Search location or meta',
-          //           border: InputBorder.none,
-          //         ),
-          //       ),
-          //       trailing: GestureDetector(
-          //         onTap: () {},
-          //         child: Icon(
-          //           Icons.close,
-          //           size: 18,
-          //           color: greyColor,
-          //         ),
-          //       ),
-          //     ),
-          //   ),
-          // ),
-        ],
-      ),
-    );
+          );
   }
 }
