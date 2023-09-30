ad90d5f1b52466920358dda59fc9d6015e73d11b
Sam Victor <iamvictorsam@gmail.com>
2023-09-30 10:45:02 +0100

    fix

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -2,7 +2,6 @@ import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:heavy_metals/bottomNavigation.dart';
 import 'package:heavy_metals/constant.dart';
-import 'package:heavy_metals/presentation/screens/home.dart';
 
 class InitScreen extends StatefulWidget {
   static String routeName = '/initScreen';
