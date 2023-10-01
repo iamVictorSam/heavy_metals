cd24ae2b66e2591f302bdba2e89d3d77a3ebc57e
Sam Victor <iamvictorsam@gmail.com>
2023-10-01 10:46:12 +0100

    fix

---
+++ lib/presentation/screens/demography/demography.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DemographyScreen extends StatelessWidget {
+  const DemographyScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
