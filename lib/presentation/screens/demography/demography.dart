b9c264fb9c5d8e3b26d76625879a2394a64f7517
Sam Victor <iamvictorsam@gmail.com>
2023-10-08 14:19:59 +0100

    fix

--- lib/presentation/screens/demography/demography.dart
+++ lib/presentation/screens/demography/demography.dart
@@ -33,29 +33,29 @@ class DemographyScreen extends StatelessWidget {
     );
   }
 
-  List<PieSeries<ChartSampleData, String>> _getRadiusPieSeries(element) {
-    print(element);
-    return <PieSeries<ChartSampleData, String>>[
-      PieSeries<ChartSampleData, String>(
-          dataSource: <ChartSampleData>[
-            ChartSampleData(x: 'Argentina', y: 505370, text: '45%'),
-            ChartSampleData(x: 'Belgium', y: 551500, text: '53.7%'),
-            ChartSampleData(x: 'Cuba', y: 312685, text: '59.6%'),
-            ChartSampleData(x: 'Dominican Republic', y: 350000, text: '72.5%'),
-            ChartSampleData(x: 'Egypt', y: 301000, text: '85.8%'),
-            ChartSampleData(x: 'Kazakhstan', y: 300000, text: '90.5%'),
-            ChartSampleData(x: 'Somalia', y: 357022, text: '95.6%')
-          ],
-          xValueMapper: (ChartSampleData data, _) => data.x as String,
-          yValueMapper: (ChartSampleData data, _) => data.y,
-          dataLabelMapper: (ChartSampleData data, _) => data.x as String,
-          startAngle: 100,
-          endAngle: 100,
-          pointRadiusMapper: (ChartSampleData data, _) => data.text,
-          dataLabelSettings: const DataLabelSettings(
-              isVisible: true, labelPosition: ChartDataLabelPosition.outside))
-    ];
-  }
+  // List<PieSeries<ChartSampleData, String>> _getRadiusPieSeries(element) {
+  //   print(element);
+  //   return <PieSeries<ChartSampleData, String>>[
+  //     PieSeries<ChartSampleData, String>(
+  //         dataSource: <ChartSampleData>[
+  //           ChartSampleData(x: 'Argentina', y: 505370, text: '45%'),
+  //           ChartSampleData(x: 'Belgium', y: 551500, text: '53.7%'),
+  //           ChartSampleData(x: 'Cuba', y: 312685, text: '59.6%'),
+  //           ChartSampleData(x: 'Dominican Republic', y: 350000, text: '72.5%'),
+  //           ChartSampleData(x: 'Egypt', y: 301000, text: '85.8%'),
+  //           ChartSampleData(x: 'Kazakhstan', y: 300000, text: '90.5%'),
+  //           ChartSampleData(x: 'Somalia', y: 357022, text: '95.6%')
+  //         ],
+  //         xValueMapper: (ChartSampleData data, _) => data.x as String,
+  //         yValueMapper: (ChartSampleData data, _) => data.y,
+  //         dataLabelMapper: (ChartSampleData data, _) => data.x as String,
+  //         startAngle: 100,
+  //         endAngle: 100,
+  //         pointRadiusMapper: (ChartSampleData data, _) => data.text,
+  //         dataLabelSettings: const DataLabelSettings(
+  //             isVisible: true, labelPosition: ChartDataLabelPosition.outside))
+  //   ];
+  // }
 }
 
 class SalesData {
