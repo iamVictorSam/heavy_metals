7b1fe40df2380799c325c9f1f678a22f6fe685b1
Sam Victor <iamvictorsam@gmail.com>
2023-10-12 14:20:59 +0100

    fix

--- lib/presentation/screens/demography/demography.dart
+++ lib/presentation/screens/demography/demography.dart
@@ -33,6 +33,65 @@ class DemographyScreen extends StatelessWidget {
     );
   }
 
+  List<PieSeries<ChartSampleData, String>> _getRadiusPieSeries(
+      MetalsModel metal) {
+    // Calculate the total for all metals at this location
+    double total = double.parse(metal.cd) +
+        double.parse(metal.pb) +
+        double.parse(metal.ni) +
+        double.parse(metal.hg) +
+        double.parse(metal.cr) +
+        double.parse(metal.zn);
+
+    // Calculate the percentage for each metal
+    double cdPercentage = (double.parse(metal.cd) / total) * 100;
+    double pbPercentage = (double.parse(metal.pb) / total) * 100;
+    double niPercentage = (double.parse(metal.ni) / total) * 100;
+    double hgPercentage = (double.parse(metal.hg) / total) * 100;
+    double crPercentage = (double.parse(metal.cr) / total) * 100;
+    double znPercentage = (double.parse(metal.zn) / total) * 100;
+
+    return <PieSeries<ChartSampleData, String>>[
+      PieSeries<ChartSampleData, String>(
+        dataSource: <ChartSampleData>[
+          ChartSampleData(
+              x: 'Cd',
+              y: cdPercentage,
+              text: '${cdPercentage.toStringAsFixed(2)}%'),
+          ChartSampleData(
+              x: 'Pb',
+              y: pbPercentage,
+              text: '${pbPercentage.toStringAsFixed(2)}%'),
+          ChartSampleData(
+              x: 'Ni',
+              y: niPercentage,
+              text: '${niPercentage.toStringAsFixed(2)}%'),
+          ChartSampleData(
+              x: 'Hg',
+              y: hgPercentage,
+              text: '${hgPercentage.toStringAsFixed(2)}%'),
+          ChartSampleData(
+              x: 'Cr',
+              y: crPercentage,
+              text: '${crPercentage.toStringAsFixed(2)}%'),
+          ChartSampleData(
+              x: 'Zn',
+              y: znPercentage,
+              text: '${znPercentage.toStringAsFixed(2)}%'),
+        ],
+        xValueMapper: (ChartSampleData data, _) => data.x as String,
+        yValueMapper: (ChartSampleData data, _) => data.y,
+        dataLabelMapper: (ChartSampleData data, _) => data.text,
+        startAngle: 100,
+        endAngle: 100,
+        dataLabelSettings: const DataLabelSettings(
+          isVisible: true,
+          labelPosition: ChartDataLabelPosition.outside,
+        ),
+      ),
+    ];
+  }
+
   // List<PieSeries<ChartSampleData, String>> _getRadiusPieSeries(element) {
   //   print(element);
   //   return <PieSeries<ChartSampleData, String>>[
