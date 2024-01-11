d4dd33d96f6bceb94b7bd635c5dce0c3d4ae667d
Sam Victor <iamvictorsam@gmail.com>
2024-01-11 10:38:40 +0100

    changed color

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-const kPrimaryColor = Color(0xff09A306);
+const kPrimaryColor = Colors.blue;
+// const kPrimaryColor = Color(0xff09A306);
 const kGrey = Color(0xff9c9c9ccc);
 const kWhite = Color(0xffFFFFFF);
 // #9C9C9CCC
